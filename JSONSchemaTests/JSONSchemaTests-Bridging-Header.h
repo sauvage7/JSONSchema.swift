e0e0cf08d3a61cc2730e1aacf8b611033f48ff4d
sauvage7 <gabriel.hoehener@iml.unibe.ch>
2016-02-29 15:30:45 +0100

    Add Bridging Header

---
+++ JSONSchemaTests/JSONSchemaTests-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  JSONSchemaTests-Bridging-Header.h
+//  JSONSchema
+//
+//  Created by Gabriel Höhener on 29.02.16.
+//  Copyright © 2016 Cocode. All rights reserved.
+//
+
+#ifndef JSONSchemaTests_Bridging_Header_h
+#define JSONSchemaTests_Bridging_Header_h
+
+#import "BaseDynamicSwiftTestCase.h"
+
+#endif /* JSONSchemaTests_Bridging_Header_h */
